a40002fad2e14b64130e8386f18f50ed0a85bba5
Daniel Micay <danielmicay@gmail.com>
2020-10-14 19:16:59 -0400

    SystemUIGoogle -> SystemUI

--- device.mk
+++ device.mk
@@ -780,7 +780,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUIGoogle
+    SystemUI
 
 # Enable stats logging in LMKD
 TARGET_LMKD_STATS_LOG := true
