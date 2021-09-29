cc6442825c75fdd77e780ca3bf23d6a381656cfe
flawedworld <flawedworld@flawed.world>
2021-09-29 04:46:28 +0100

    Use APNs and CarrierConfig from APV

--- sunfish.mk
+++ sunfish.mk
@@ -3,3 +3,5 @@ $(call inherit-product, device/google/sunfish/aosp_sunfish.mk)
 
 # Remove AOSP prefix from product name
 PRODUCT_NAME := sunfish
+# Tell build system not to bundle sample APNs from AOSP
+OEM_APNS := true
