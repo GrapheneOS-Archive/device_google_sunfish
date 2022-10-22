9f46f6a6130eb5559d5d9f9cc9d984caa252351d
Daniel Micay <danielmicay@gmail.com>
2022-10-22 05:17:50 -0400

    disable kernel module stripping

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -201,6 +201,11 @@ DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/sunfish/device_framewo
 # Use mke2fs to create ext4 images
 TARGET_USES_MKE2FS := true
 
+BOARD_DO_NOT_STRIP_RECOVERY_MODULES := true
+BOARD_DO_NOT_STRIP_VENDOR_MODULES := true
+BOARD_DO_NOT_STRIP_VENDOR_RAMDISK_MODULES := true
+BOARD_DO_NOT_STRIP_VENDOR_KERNEL_RAMDISK_MODULES := true
+
 # Kernel modules
 ifeq (,$(filter-out sunfish_kasan, $(TARGET_PRODUCT)))
 BOARD_VENDOR_KERNEL_MODULES += \
